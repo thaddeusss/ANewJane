6bfa770e58dcab81d99949fa68d1639d1d934209
breykerr <106961694+breykerr@users.noreply.github.com>
2023-06-10 18:36:44 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import home from "./pages/home/home";
-import about from "./pages/about/about";
-import portfolio from "./pages/portfolio/portfolio";
-import contact from "./pages/contact/contact";
+import Home from "./pages/home/home";
+import About from "./pages/about/about";
+import Portfolio from "./pages/portfolio/portfolio";
+import Contact from "./pages/contact/contact";
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route imdex element={<home />} />
-        <Route path="about" element={<about />} />
-        <Route path="portfolio" element={<portfolio />} />
-        <Route path="contact" element={<contact />} />
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
