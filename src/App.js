2944dddd76212d56d0c51312f23148bb09a71050
breykerr <106961694+breykerr@users.noreply.github.com>
2023-07-03 11:27:46 +0600

    added mainroutes etc

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/home/home";
-import About from "./pages/about/about";
-import Portfolio from "./pages/portfolio/portfolio";
-import Contact from "./pages/contact/contact";
 import Themes from "./components/Themes";
 import "./index.css";
+import MainRoutes from "./MainRoutes";
 // import BackAnimate from "./components/BackAnimate";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
+    <>
       <Themes />
+      <Navbar />
+      <MainRoutes />
       {/* <BackAnimate /> */}
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="portfolio" element={<Portfolio />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
