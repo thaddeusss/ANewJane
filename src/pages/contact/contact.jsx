fcccf53ddf8727f3b6d63f2d0406966f1ce4a104
breykerr <106961694+breykerr@users.noreply.github.com>
2023-07-06 02:39:08 +0600

    change contact

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -8,8 +8,10 @@ import {
   FaHome,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
 const contact = () => {
+  const handleClickImg = () => {
+    window.open("https://www.youtube.com/watch?v=nyuo9-OjNNg", "_blank");
+  };
   return (
     <section className="contact section">
       <div className="contact__content container grid">
@@ -22,7 +24,6 @@ const contact = () => {
           >
             <FaInstagram />
           </a>
-
           <a
             href="https://t.me/zholdubaevva"
             className="contact__social-link"
@@ -31,7 +32,6 @@ const contact = () => {
           >
             <FaTelegramPlane />
           </a>
-
           <a
             href="https://www.youtube.com/@zholdubaevva"
             className="contact__social-link"
@@ -40,7 +40,6 @@ const contact = () => {
           >
             <FaYoutube />
           </a>
-
           <a
             href="https://www.tiktok.com/@zholdubaevva"
             className="contact__social-link"
@@ -50,9 +49,7 @@ const contact = () => {
             <FaTiktok />
           </a>
         </div>
-
         <div className="contact__img"></div>
-
         <div className="contact__data">
           <h2 className="data__title">
             Con<span>tact</span>
@@ -66,16 +63,13 @@ const contact = () => {
           {/* <div className="contact__info">
               <div className="info__item">
                 <FaInstagram className="info__icon" />
-
                 <div>
                   <span className="info__title">Instagram</span>
                   <h4 className="info__desc">bekaspirit@gmail.com</h4>
                 </div>
               </div>
-
               <div className="info__item">
                 <FaPhoneSquareAlt className="info__icon" />
-
                 <div>
                   <span className="info__title">Call me</span>
                   <h4 className="info__desc">+996 700 997 290</h4>
@@ -89,7 +83,6 @@ const contact = () => {
             </span>
           </Link>
         </div>
-
         {/* <form className="contact__form">
             <div className="form__input-group">
               <div className="form__input-div">
@@ -99,7 +92,6 @@ const contact = () => {
                   className="form__control"
                 />
               </div>
-
               <div className="form__input-div">
                 <input
                   type="email"
@@ -107,7 +99,6 @@ const contact = () => {
                   className="form__control"
                 />
               </div>
-
               <div className="form__input-div">
                 <input
                   type="text"
@@ -116,7 +107,6 @@ const contact = () => {
                 />
               </div>
             </div>
-
             <div className="form__input-div">
               <textarea
                 placeholder="Your Message"
@@ -134,16 +124,9 @@ const contact = () => {
       <div className="error">
         {/* <div className="error-box">
           <img
-            className="world"
-            src="https://i.pinimg.com/236x/f0/a2/26/f0a226871a4263eabee2e578a03d7742.jpg"
-            alt=""
-          />
-        </div> */}
-        {/* <div className="error-box">
-          <img
-            className="world"
-            src="https://i.pinimg.com/236x/a8/14/ae/a814aefdb866ee497338ec64e45433f6.jpg"
-            alt=""
+          className="world"
+          src="https://i.pinimg.com/236x/a8/14/ae/a814aefdb866ee497338ec64e45433f6.jpg"
+          alt=""
           />
         </div> */}
         <div className="error-box">
@@ -154,10 +137,14 @@ const contact = () => {
           />
         </div>
         <div className="error-box">
+          <p>ClickMe</p>
           <img
             className="world"
-            src="https://i.pinimg.com/236x/c1/51/41/c15141a7b71274eaece9537ae4540071.jpg"
-            alt=""
+            src="https://i.pinimg.com/564x/66/07/ee/6607eea3d5e8761a112d00f39ab068c9.jpg"
+            alt="CLICK ME"
+            onClick={() => {
+              handleClickImg();
+            }}
           />
         </div>
         <div className="error-box">
@@ -167,9 +154,15 @@ const contact = () => {
             alt=""
           />
         </div>
+        {/* <div className="error-box">
+          <img
+            className="world"
+            src="https://i.pinimg.com/236x/c1/51/41/c15141a7b71274eaece9537ae4540071.jpg"
+            alt=""
+          />
+        </div> */}
       </div>
     </section>
   );
 };
-
 export default contact;
