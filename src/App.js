b5394dafde0fecf2a98f521e76f9eaafde3a3aac
breykerr <106961694+breykerr@users.noreply.github.com>
2023-07-06 03:41:16 +0600

    deploy on github, fixed url addres

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/home/home";
 import About from "./pages/about/about";
@@ -11,16 +11,16 @@ import "./index.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Themes />
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="portfolio" element={<Portfolio />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
